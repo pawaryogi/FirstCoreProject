232381018170f9e3dec0c4789ee65a94ce35b04e
Yogesh Pawar <pawaryogi29@gmail.com>
2022-12-27 16:18:23 +0530

    Added Test controller

---
+++ EmployeeManagement/Controllers/TestController.cs
@@ -0,0 +1,26 @@
+﻿using EmployeeManagement.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Controllers
+{
+    public class TestController : Controller
+    {
+        private IEmployeeInterface _EmpRepository;
+
+        public TestController(IEmployeeInterface emprepository)
+        {
+            _EmpRepository = emprepository;
+        }
+
+        public string Index()
+        {
+            return _EmpRepository.GetEmployee(2).Name.ToString();
+        }
+
+
+    }
+}
